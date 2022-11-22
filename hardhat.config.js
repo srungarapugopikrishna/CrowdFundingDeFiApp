8be619000522cc0cc8109fc4158daafe76810856
Gopi Krishna <srungarapugopikrishna@gmail.com>
2022-11-22 20:14:41 +0530

    new changes

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,8 +5,8 @@ module.exports = {
   solidity: "0.8.17",
   networks: {
     goerli: {
-      gas: 2100000,
-      gasPrice: 8000000000,
+      // gas: 2100000,
+      // gasPrice: 8000000000,
       // url: process.env.GOERLI_URL,
       // accounts: [process.env.ACC_PRIVATE_KEY],
       url: "https://eth-goerli.g.alchemy.com/v2/VjSnVmpD6gRO3EqVwps-TUbn2eLEtSn-",
